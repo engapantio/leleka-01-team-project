92e641f6f2a796455de824105bcd5006d82ec08d
engapantio <bohdankozar@zohomail.eu>
2026-01-07 22:33:12 +0100

    set up login and refactor middleware to proxy

---
+++ lib/store/authStore.ts
@@ -0,0 +1,32 @@
+import { create } from 'zustand';
+import { User } from '@/types/user';
+
+interface AuthStore {
+  user: User | null;
+  isAuthenticated: boolean;
+  setUser: (user: User | null) => void;
+  clearAuth: () => void;
+  updateUser: (user: Partial<User>) => void;
+}
+
+export const useAuthStore = create<AuthStore>()(set => ({
+  user: null,
+  isAuthenticated: false,
+
+  setUser: user =>
+    set({
+      user,
+      isAuthenticated: true,
+    }),
+
+  clearAuth: () =>
+    set({
+      user: null,
+      isAuthenticated: false,
+    }),
+
+  updateUser: updatedUser =>
+    set(state => ({
+      user: state.user ? { ...state.user, ...updatedUser } : null,
+    })),
+}));
---
+++ proxy.ts
@@ -0,0 +1,73 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+import { parse } from 'cookie';
+import { checkSession } from './lib/api/serverApi';
+
+const publicRoutes = ['/auth'];
+const privateRoutes = ['/journey', '/diary', '/profile'];
+
+export async function proxy(request: NextRequest) {
+  const cookiesStore = await cookies();
+  const { pathname } = request.nextUrl;
+  const accessToken = cookiesStore.get('accessToken')?.value;
+  const refreshToken = cookiesStore.get('refreshToken')?.value;
+
+  const isPublicRoute = publicRoutes.some(route => pathname.startsWith(route));
+  const isPrivateRoute = privateRoutes.some(route => pathname.startsWith(route));
+
+  if (!accessToken) {
+    if (refreshToken) {
+      const data = await checkSession();
+      const setCookie = data.headers['set-cookie'];
+      if (setCookie) {
+        const cookiesArray = Array.isArray(setCookie) ? setCookie : [setCookie];
+
+        for (const cookie of cookiesArray) {
+          const parsed = parse(cookie);
+          const options = {
+            expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
+            path: parsed.Path,
+            maxAge: Number(parsed['Max-Age']),
+          };
+          if (parsed.accessToken) {
+            cookiesStore.set('accessToken', parsed.accessToken, options);
+          }
+          if (parsed.refreshToken) {
+            cookiesStore.set('refreshToken', parsed.refreshToken, options);
+          }
+        }
+        if (isPublicRoute) {
+          return NextResponse.redirect(new URL('/', request.url), {
+            headers: {
+              Cookie: cookiesStore.toString(),
+            },
+          });
+        }
+        if (isPrivateRoute) {
+          return NextResponse.next({
+            headers: {
+              Cookie: cookiesStore.toString(),
+            },
+          });
+        }
+      }
+    }
+    if (isPublicRoute) {
+      return NextResponse.next();
+    }
+    if (isPrivateRoute) {
+      return NextResponse.redirect(new URL('/auth/login', request.url));
+    }
+  }
+
+  if (isPublicRoute) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+  if (isPrivateRoute) {
+    return NextResponse.next();
+  }
+}
+
+export const config = {
+  matcher: ['/profile/:path*', '/auth/:path*', '/journey', '/diary'],
+};
