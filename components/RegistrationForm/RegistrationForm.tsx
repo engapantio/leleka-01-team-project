1462cca953a2f293400b0ef3a63b03845594cfb7
Alina Y. <devadevam.tod@gmail.com>
2026-01-10 00:08:27 +0200

    register mobile styles

--- components/RegistrationForm/RegistrationForm.tsx
+++ components/RegistrationForm/RegistrationForm.tsx
@@ -1,29 +1,63 @@
 'use client';
-
+import Link from 'next/link';
 import { useId } from 'react';
 import { Formik, Form, Field } from 'formik';
-// import css from './RegistrationForm.module.css'
+import css from './RegistrationForm.module.css';
 
 const RegistrationForm = () => {
   const fieldId = useId();
   return (
     <Formik initialValues={{}} onSubmit={() => {}}>
-      <Form>
-        <fieldset>
-          <legend>Реєстрація</legend>
-
-          <label htmlFor={`${fieldId}-name`}>Імʼя*</label>
-          <Field type="text" name="name" id={`${fieldId}-name`} />
-
-          <label htmlFor={`${fieldId}-email`}>Пошта*</label>
-          <Field type="email" name="email" id={`${fieldId}-email`} />
-
-          <label htmlFor={`${fieldId}-password`}>Пароль*</label>
-          <Field type="password" name="password" id={`${fieldId}-password`} />
+      <Form className={css.form}>
+        <fieldset className={css.fieldset}>
+          <legend className={css.title}>Реєстрація</legend>
+          <div className={css.field}>
+            <label className={css.label} htmlFor={`${fieldId}-name`}>
+              Імʼя*
+            </label>
+            <Field
+              className={css.input}
+              type="text"
+              name="name"
+              id={`${fieldId}-name`}
+              placeholder="Ваше імʼя"
+            />
+          </div>
+          <div className={css.field}>
+            <label className={css.label} htmlFor={`${fieldId}-email`}>
+              Пошта*
+            </label>
+            <Field
+              className={css.input}
+              type="email"
+              name="email"
+              id={`${fieldId}-email`}
+              placeholder="hello@leleka.com"
+            />
+          </div>
+          <div className={css.field}>
+            <label className={css.label} htmlFor={`${fieldId}-password`}>
+              Пароль*
+            </label>
+            <Field
+              className={css.input}
+              type="password"
+              name="password"
+              id={`${fieldId}-password`}
+              placeholder="********"
+            />
+          </div>
         </fieldset>
 
-        <button type="submit">Зареєструватись</button>
-        <p>Вже маєте аккаунт? Увійти</p>
+        <button type="submit" className={css.button}>
+          Зареєструватись
+        </button>
+        <p className={css.login}>
+          Вже маєте аккаунт?
+          <Link href="/" aria-label="Login" className={css.link}>
+            Увійти
+          </Link>
+        </p>
       </Form>
     </Formik>
   );
