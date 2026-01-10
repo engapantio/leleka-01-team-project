d478291d780c2f284cc3be147b067d9f89a43872
Alina Y. <devadevam.tod@gmail.com>
2026-01-10 22:26:36 +0200

    registration actions

--- components/RegistrationForm/RegistrationForm.tsx
+++ components/RegistrationForm/RegistrationForm.tsx
@@ -1,13 +1,68 @@
 'use client';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import * as Yup from 'yup';
+import toast, { Toaster } from 'react-hot-toast';
 import { useId } from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, FormikHelpers, ErrorMessage } from 'formik';
 import css from './RegistrationForm.module.css';
+import { register, RegistrationDetails } from '@/lib/api/clientApi';
+
+const RegisterSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, ' Імʼя занадто коротке.')
+    .max(32, 'Імʼя занадто довге.')
+    .required('Будь ласка, введіть імʼя.'),
+  email: Yup.string()
+    .email('Невірно введена пошта.')
+    .required('Будь ласка, введіть пошту.')
+    .max(64, 'Пошта занадто довга.'),
+  password: Yup.string()
+    .min(8, 'Пароль занадто короткий.')
+    .max(128, 'Пароль занадто довгий.')
+    .required('Будь ласка, введіть пароль.'),
+});
+
+const initialValues: RegistrationDetails = {
+  name: '',
+  email: '',
+  password: '',
+};
 
 const RegistrationForm = () => {
+  const router = useRouter();
+  // const [error, setError] = useState('');
   const fieldId = useId();
+  const handleSubmit = async (
+    values: RegistrationDetails,
+    actions: FormikHelpers<RegistrationDetails>
+  ) => {
+    console.log('Reg data:', values);
+    try {
+      const res = await register(values);
+      if (res) {
+        toast.success('Реєстрація успішна!');
+        router.push(''); // ONBORDING
+      } else {
+        // setError('Виникла помилка при реєстрації.');
+        toast.error('Виникла помилка при реєстрації.', {
+          position: 'top-left',
+        });
+      }
+      actions.resetForm();
+    } catch (err) {
+      // setError(
+      //   (err as ApiError).response?.data?.error ??
+      //     (err as ApiError).message ??
+      //     'Виникла помилка при реєстрації.'
+      // );
+      toast.error('Виникла помилка при реєстрації.', {
+        position: 'top-left',
+      });
+    }
+  };
   return (
-    <Formik initialValues={{}} onSubmit={() => {}}>
+    <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={RegisterSchema}>
       <Form className={css.form}>
         <fieldset className={css.fieldset}>
           <legend className={css.title}>Реєстрація</legend>
@@ -22,6 +77,7 @@ const RegistrationForm = () => {
               id={`${fieldId}-name`}
               placeholder="Ваше імʼя"
             />
+            <ErrorMessage name="name" component="span" className={css.error} />
           </div>
           <div className={css.field}>
             <label className={css.label} htmlFor={`${fieldId}-email`}>
@@ -34,6 +90,7 @@ const RegistrationForm = () => {
               id={`${fieldId}-email`}
               placeholder="hello@leleka.com"
             />
+            <ErrorMessage name="email" component="span" className={css.error} />
           </div>
           <div className={css.field}>
             <label className={css.label} htmlFor={`${fieldId}-password`}>
@@ -46,6 +103,7 @@ const RegistrationForm = () => {
               id={`${fieldId}-password`}
               placeholder="********"
             />
+            <ErrorMessage name="password" component="span" className={css.error} />
           </div>
         </fieldset>
 
@@ -58,6 +116,7 @@ const RegistrationForm = () => {
             Увійти
           </Link>
         </p>
+        <Toaster />
       </Form>
     </Formik>
   );
