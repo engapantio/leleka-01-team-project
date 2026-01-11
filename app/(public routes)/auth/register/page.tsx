7c18815f04346289be39b8a1b032e7fd26589659
Bohdan Kozar <203163016+engapantio@users.noreply.github.com>
2026-01-11 15:14:09 +0100

    Remove registration page component

--- app/(public routes)/auth/register/page.tsx
+++ app/(public routes)/auth/register/page.tsx
@@ -1,12 +1,1 @@
-import AuthContainer from '@/components/AuthContainer/AuthContainer';
-import RegistrationForm from '@/components/RegistrationForm/RegistrationForm';
 
-const Register = () => {
-  return (
-    <AuthContainer imagePath="/register.jpg">
-      <RegistrationForm />
-    </AuthContainer>
-  );
-};
-
-export default Register;
