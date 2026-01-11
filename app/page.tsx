ad0618f791c207a413329f75512ee4f042901c05
Daria Chaiun <dariachiun@gmail.com>
2026-01-11 19:21:51 +0200

    momtip&feelingcheck

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
-const Dashboard = () => {
-  return <div>Dashboard</div>;
-};
+'use client';
 
-export default Dashboard;
+import MomTipCard from '@/components/MomTipCard/MomTipCard';
+import FeelingCheckCard from '@/components/FeelingCheckCard/FeelingCheckCard';
+
+export default function Home() {
+  return (
+    <section
+      style={{
+        display: 'flex',
+        alignItems: 'flex-start',
+        width: '100%',
+        gap: 32,
+        flexWrap: 'wrap',
+      }}
+    >
+      <MomTipCard adviceForMom="Пий воду і відпочивай ❤️" />
+      <FeelingCheckCard />
+    </section>
+  );
+}
