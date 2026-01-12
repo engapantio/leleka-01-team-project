d896123fba4489733d07ba8efcf4177a43034ef8
Skwi <andrewdoluda@gmail.com>
2026-01-12 17:28:45 +0100

    layout
    Added Breadcrumbs
    Added and fixed Sidebar  (position:static;)
    Some fixes in Breadcrumbs

--- components/Breadcrumbs/Breadcrumbs.tsx
+++ components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,50 +1,60 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import React from "react";
-import css from "./Breadcrumbs.module.css";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+import css from './Breadcrumbs.module.css';
 type Props = {
   currentLabel?: string;
 };
 
 const breadcrumbNameMap: Record<string, string> = {
-  "/diary": "Щоденник",
-  "/journey": "Подорож",
-  "/profile": "Профіль",
+  '/diary': 'Щоденник',
+  '/journey': 'Подорож',
+  '/profile': 'Профіль',
 };
 
 export default function Breadcrumbs({ currentLabel }: Props) {
   const pathname = usePathname();
 
-  if (pathname.startsWith("/auth")) {
+  if (pathname.startsWith('/auth')) {
     return null;
   }
 
-  if (pathname === "/") {
+  if (pathname === '/') {
     return (
       <nav className={css.breadcrumbs} aria-label="Breadcrumb">
         <ul className={css.breadcrumbsList}>
-        <li className={css.item}>
-          <Link href="/" className={css.link}>Лелека</Link>
-        </li>
-        <li className={css.separatorItem}>
-          <svg width={24} height={24} className={css.separatorIcon}>
-            <use href="/sprite.svg#breadcrumbs-icon" />
-          </svg>
-        </li>
-        <li className={css.item}>
-          <Link href="/" className={css.current}>Miй день</Link>
-        </li>
+          <li className={css.item}>
+            <Link href="/" className={css.link}>
+              Лелека
+            </Link>
+          </li>
+          <li className={css.separatorItem}>
+            <svg
+              width={24}
+              height={24}
+              className={css.separatorIcon}
+              aria-hidden="true"
+              viewBox="0 0 32 32"
+            >
+              <use href="/sprite.svg#breadcrumbs-icon" />
+            </svg>
+          </li>
+          <li className={css.item}>
+            <Link href="/" className={css.current}>
+              Miй день
+            </Link>
+          </li>
         </ul>
       </nav>
     );
   }
 
-  const pathSegments = pathname.split("/").filter(Boolean);
+  const pathSegments = pathname.split('/').filter(Boolean);
 
   const crumbs = pathSegments.map((segment, index) => {
-    const href = "/" + pathSegments.slice(0, index + 1).join("/");
+    const href = '/' + pathSegments.slice(0, index + 1).join('/');
     const isLast = index === pathSegments.length - 1;
 
     let label = breadcrumbNameMap[href];
@@ -67,13 +77,19 @@ export default function Breadcrumbs({ currentLabel }: Props) {
         </Link>
       </li>
 
-      {crumbs.map((crumb) => (
+      {crumbs.map(crumb => (
         <React.Fragment key={crumb.href}>
-         <li className={css.separatorItem}>
-          <svg width={24} height={24} className={css.separatorIcon}>
-            <use href="/sprite.svg#breadcrumbs-icon" />
-          </svg>
-        </li>
+          <li className={css.separatorItem}>
+            <svg
+              width={24}
+              height={24}
+              className={css.separatorIcon}
+              aria-hidden="true"
+              viewBox="0 0 32 32"
+            >
+              <use href="/sprite.svg#breadcrumbs-icon" />
+            </svg>
+          </li>
 
           <li className={css.item}>
             {crumb.isLast ? (
