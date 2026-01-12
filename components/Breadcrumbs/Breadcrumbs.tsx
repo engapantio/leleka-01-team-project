3d237c8042fecc469ccec158501162d2a4e32010
Skwi <andrewdoluda@gmail.com>
2026-01-12 16:19:04 +0100

    Test Component (Breadcrumbs)
    Initial version
    Will be updated

---
+++ components/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import css from "./Breadcrumbs.module.css";
+import Image from "next/image";
+type Props = {
+  currentLabel?: string;
+};
+
+const breadcrumbNameMap: Record<string, string> = {
+  "/diary": "Щоденник",
+  "/journey": "Подорож",
+  "/profile": "Профіль",
+};
+
+export default function Breadcrumbs({ currentLabel }: Props) {
+  const pathname = usePathname();
+
+  if (pathname.startsWith("/auth")) {
+    return null;
+  }
+
+  if (pathname === "/") {
+    return (
+      <nav className={css.breadcrumbs} aria-label="Breadcrumb">
+        <ul className={css.breadcrumbsList}>
+        <li className={css.item}>
+          <Link href="/" className={css.link}>Лелека</Link>
+        </li>
+        <li className={css.separatorItem}>
+          <svg width={24} height={24} className={css.separatorIcon}>
+            <use href="/sprite.svg#breadcrumbs-icon" />
+          </svg>
+        </li>
+        <li className={css.item}>
+          <Link href="/" className={css.current}>Miй день</Link>
+        </li>
+        </ul>
+      </nav>
+    );
+  }
+
+  const pathSegments = pathname.split("/").filter(Boolean);
+
+  const crumbs = pathSegments.map((segment, index) => {
+    const href = "/" + pathSegments.slice(0, index + 1).join("/");
+    const isLast = index === pathSegments.length - 1;
+
+    let label = breadcrumbNameMap[href];
+
+    if (!label && isLast && currentLabel) {
+      label = currentLabel;
+    }
+    if (!label) {
+      label = segment;
+    }
+
+    return { href, label, isLast };
+  });
+
+  return (
+    <nav className={css.breadcrumbs} aria-label="Breadcrumb">
+      <li className={css.item}>
+        <Link href="/" className={css.link}>
+          Лелека
+        </Link>
+      </li>
+
+      {crumbs.map((crumb) => (
+        <React.Fragment key={crumb.href}>
+          <Image
+            width={24}
+            height={24}
+            src="/chevron_right.svg"
+            alt=""
+            className={css.separatorIcon}
+          />
+
+          <li className={css.item}>
+            {crumb.isLast ? (
+              <span className={css.current}>{crumb.label}</span>
+            ) : (
+              <Link href={crumb.href} className={css.link}>
+                {crumb.label}
+              </Link>
+            )}
+          </li>
+        </React.Fragment>
+      ))}
+    </nav>
+  );
+}
