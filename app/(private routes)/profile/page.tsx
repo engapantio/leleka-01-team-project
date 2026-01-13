3dc86255405e570379fff9784430f2a824f8040e
Oleksii Levchenko <neonator3000@gmail.com>
2026-01-13 21:28:33 +0200

    test commit

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client'
 
 import ProfileAvatar from "@/components/ProfileAvatar/ProfileAvatar";
@@ -8,25 +7,6 @@ import { User } from "@/types/user";
 import { getCurrentUser } from "@/lib/api/clientApi";
 
 
-const fakeUser: User = {
-  id: "1f143f82-bb18-4795-af3d-52ccf272ffd8",
-  name: "testName",
-  email: "test@test.com",
-  gender: "boy",
-  dueDate: "2026-02-12",
-  avatar: "https://res.cloudinary.com/dwvvx8vqk/image/upload/default-avatar_zkouxm.png",
-  createdAt: "2026-01-12T20:38:59.722Z",
-  updatedAt: "2026-01-12T20:38:59.722Z"
-=======
-export default async function ProfilePage(props: {
-  params: Promise<{ [key: string]: string | string[] | undefined }>;
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}) {
-  await props.params;
-  await props.searchParams;
-  return <div>Empty Placeholder</div>;
->>>>>>> origin/main
-}
 
 
 export default function ProfilePage() {
@@ -46,11 +26,11 @@ export default function ProfilePage() {
         fetchData()
     }, [])
 
-
+// замінити пропс
     return (
         <>
-            <ProfileAvatar user={fakeUser} />
-            <ProfileEditForm dataUser={fakeUser} />
+            <ProfileAvatar user={dataUser} />
+            <ProfileEditForm dataUser={dataUser} />
         </>
     )
 }
