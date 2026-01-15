86c64cde196254ebe444d3b624e6a53d566b5464
OlhaMasl <maslakok@gmail.com>
2026-01-15 18:42:22 +0200

    Add css for mom card

--- components/JourneyDetails/JourneyDetails.tsx
+++ components/JourneyDetails/JourneyDetails.tsx
@@ -43,7 +43,7 @@ const JourneyDetails = ({
               </div>
               {babyData.analogy && <p className={styles.analogy}>{babyData.analogy}</p>}
             </div>
-
+            <div>
             <p className={styles.babyText}>{babyData.babyActivity}</p>
             <p className={styles.babyText}>{babyData.babyDevelopment}</p>
             <p className={styles.babyText}>{babyData.momDailyTips.join(' ')}</p>
@@ -54,35 +54,53 @@ const JourneyDetails = ({
                 <h3 className={styles.interestingFactTitle}>Цікавий факт тижня</h3>
               </div>
               <p>{babyData.interestingFact}</p>
-            </div>
+              </div>
+              </div>
           </div>
         )}
         {MomData && selectedTab === 'mom' && (
           <div>
-            <div>
-              <h2>Як ви можете почуватись</h2>
-              <div>
-                <p>{MomData.feelingsStates[0]}</p>
-                <p>{MomData.feelingsStates[1]}</p>
-                <p>{MomData.feelingsStates[2]}</p>
+          <div className={styles.momCard}>
+            <div className={styles.momBlock}>
+              <h2 className={styles.momBlockTitle}>Як ви можете почуватись</h2>
+              <div className={styles.momBlockFeelings}>
+                <p className={styles.momBlockStiker}>{MomData.feelingsStates[0]}</p>
+                <p className={styles.momBlockStiker}>{MomData.feelingsStates[1]}</p>
+                <p className={styles.momBlockStiker}>{MomData.feelingsStates[2]}</p>
               </div>
+              <p className={styles.momBlockText}>{MomData. sensationDescr}</p>
             </div>
-            <div>
-              <h2>Поради для вашого комфорту</h2>
-              <div>
-                <h3>{MomData.comfortTips[0].category}</h3>
-                <p>{MomData.comfortTips[0].tip}</p>
+
+            <div className={styles.momBlock}>
+              <h2 className={styles.momBlockTitle}>Поради для вашого комфорту</h2>
+
+              <div className={styles.momTips}>
+                <Icon name={'icon-fork_spoon'} width={24} height={24} />
+                <div>
+                  <h3 className={styles.momTipTitle}>{MomData.comfortTips[0].category}</h3>
+                  <p className={styles.momTipText}>{MomData.comfortTips[0].tip}</p>
+                  </div>
               </div>
-              <div>
-                <h3>{MomData.comfortTips[1].category}</h3>
-                <p>{MomData.comfortTips[1].tip}</p>
+
+              <div className={styles.momTips}>
+                <Icon name={'icon-fitness_center'} width={24} height={24} />
+                <div >
+                  <h3 className={styles.momTipTitle}>{MomData.comfortTips[1].category}</h3>
+                  <p className={styles.momTipText}>{MomData.comfortTips[1].tip}</p>
+                  </div>
               </div>
-              <div>
-                <h3>{MomData.comfortTips[2].category}</h3>
-                <p>{MomData.comfortTips[2].tip}</p>
+
+              <div className={styles.momTips}>
+                <Icon name={'icon-chair'} width={24} height={24} />
+                <div>
+                  <h3 className={styles.momTipTitle}>{MomData.comfortTips[2].category}</h3>
+                  <p className={styles.momTipText}>{MomData.comfortTips[2].tip}</p>
+                </div>
               </div>
+
             </div>
-          </div>
+            </div>
+            </div>
         )}
       </div>
     </div>
