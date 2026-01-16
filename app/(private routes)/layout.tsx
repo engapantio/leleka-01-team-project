5fd3199ae31a5ac39c754ff2e35266b548761df8
Skwi <andrewdoluda@gmail.com>
2026-01-16 16:14:01 +0100

    Component (Header)
    + working burger menu
    Please check if all correct

--- app/(private routes)/layout.tsx
+++ app/(private routes)/layout.tsx
@@ -1,10 +1,10 @@
 // app/(private routes)/layout.tsx
 'use client';
-
+import { useState } from 'react';
 import { usePathname } from 'next/navigation';
 import SideBar from '@/components/SideBar/SideBar';
 import Breadcrumbs from '@/components/Breadcrumbs/Breadcrumbs';
-
+import Header from '@/components/Header/Header';
 interface PrivateLayoutProps {
   children: React.ReactNode;
 }
@@ -13,11 +13,13 @@ export default function PrivateLayout({ children }: PrivateLayoutProps) {
   const pathname = usePathname();
   const showSidebar = pathname !== '/profile/edit';
   const showBreadcrumbs = pathname !== '/profile/edit';
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
     <div className="layout-container">
+      <Header onMenuClick={() => setIsSidebarOpen(true)} />
+         {showSidebar && <SideBar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />}
       <div className="main-content-container">
-        {showSidebar && <SideBar />}
         {showBreadcrumbs && <Breadcrumbs />}
         {children}
       </div>
